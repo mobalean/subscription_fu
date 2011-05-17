e0c5abfbf2d8a648f214324dc5ca48efb2372fb2
Paul McMahon <paul@mobalean.com>
2011-05-17 17:11:29 +0900

    Rename current subscription to active subscription

--- lib/subscription_fu/models.rb
+++ lib/subscription_fu/models.rb
@@ -8,7 +8,7 @@ module SubscriptionFu
       def needs_subscription
         send(:include, InstanceMethods)
         has_many :subscriptions, :class_name => "SubscriptionFu::Subscription", :as => :subject, :dependent => :destroy
-        delegate :plan, :sponsored?, :canceled?, :prefix => :subscription, :to => :current_subscription, :allow_nil => true
+        delegate :plan, :sponsored?, :canceled?, :prefix => :subscription, :to => :active_subscription, :allow_nil => true
         delegate :plan, :prefix => :upcoming_subscription, :to => :upcoming_subscription, :allow_nil => true
       end
     end
@@ -18,18 +18,22 @@ module SubscriptionFu
         self.class.model_name.human
       end
 
-      def current_subscription
-        @current_subscription ||= subscriptions.current(Time.now).last
+      def active_subscription
+        @active_subscription ||= subscriptions.current(Time.now).last
+      end
+
+      def active_subscription?
+        !@active_subscription.nil?
       end
 
       def upcoming_subscription
-        current_subscription ? current_subscription.next_subscriptions.activated.last : nil
+        active_subscription ? active_subscription.next_subscriptions.activated.last : nil
       end
 
       def build_next_subscription(plan_key)
-        if current_subscription
+        if active_subscription
           # TODO refactor
-          subscriptions.build_for_initializing(plan_key, current_subscription.successor_start_date(plan_key), current_subscription.successor_billing_start_date, current_subscription)
+          subscriptions.build_for_initializing(plan_key, active_subscription.successor_start_date(plan_key), active_subscription.successor_billing_start_date, active_subscription)
         else
           subscriptions.build_for_initializing(plan_key)
         end
