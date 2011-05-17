c94b8d84cdfcd5643955284c13e26a3a4042a83b
Michael Reinsch <michael@mobalean.com>
2011-05-17 12:59:10 +0900

    fixing requires

--- spec/app/config/application.rb
+++ spec/app/config/application.rb
@@ -3,8 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "active_record/railtie"
 require "rails/test_unit/railtie"
-require 'subscription_fu/railtie'
-require 'subscription_fu/engine'
+require 'subscription_fu'
 
 module RailsApp
   class Application < Rails::Application
