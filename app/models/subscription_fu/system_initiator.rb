5fe81b1056f786babad088fc519a9b2567835d7b
Michael Reinsch <michael@mobalean.com>
2011-07-05 17:55:05 +0900

    added to_s to SystemInitiator, makes it easier to list it along with other users

--- app/models/subscription_fu/system_initiator.rb
+++ app/models/subscription_fu/system_initiator.rb
@@ -4,4 +4,8 @@ class SubscriptionFu::SystemInitiator < ActiveRecord::Base
   def self.paypal_sync_initiator
     find_or_create_by_name("paypal sync", :description => "Updates subscription status based on status returned by Paypal")
   end
+
+  def to_s
+    name
+  end
 end
